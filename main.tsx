f06b1de478d33cfc7da86272c4433e12aa270db2
IvanaSpeziale <139582321+IvanaSpeziale@users.noreply.github.com>
2023-11-14 15:27:24 +0100

    Add configs

--- main.tsx
+++ main.tsx
@@ -1,17 +1,13 @@
-import App from './core/components/App.tsx';
-import './core/components/index.css';
+import App from './App.tsx';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { store } from './store/store.ts';
-import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.querySelector('.container') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </Router>
   </React.StrictMode>
 );
